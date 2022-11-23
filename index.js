b245c86fee968c5330420058941cb76b6d299974
Marco Sandrini <marco.sandrini@socialfirstgames.com>
2022-11-23 23:51:51 +0200

    Switch to ncc
    Changed to ncc compilation to avoid checking in node_modules

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const core = require('@actions/core');
+const asana = require('asana');
 
-try {
-    console.log("In the action code");
-    console.log(process.env.GITHUB_EVENT_PATH);
-    const payload = require(process.env.GITHUB_EVENT_PATH);
-    console.log(payload);
-} catch (error) {
-    core.setFailed(error.message);
+async function run() {
+    try {
+        console.log("In the action code");
+        console.log(process.env.GITHUB_EVENT_PATH);
+        const payload = require(process.env.GITHUB_EVENT_PATH);
+        console.log(process.env.ASANA_PAT)
+        const client = asana.Client.create().useAccessToken(process.env.ASANA_PAT);
+        const me = await client.users.me();
+        console.log(me);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
 }
+
+// noinspection JSIgnoredPromiseFromCall
+run()
